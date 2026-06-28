f80bb7d37ad611a59b958ea0dda72002c527cc9c
dharmeshsgupta <dharmesh.pse.2024@gmail.com>
2026-06-28 18:08:22 +0530

    Fix missing schemas import in route_service

--- transport_service/app/services/route_service.py
+++ transport_service/app/services/route_service.py
@@ -1,7 +1,7 @@
 from sqlalchemy.ext.asyncio import AsyncSession
 from sqlalchemy import select
 from app.repositories.route_repository import route_repository
-from app.schemas.route import RouteCreate, RouteUpdate, RouteCompleteCreate
+from app.schemas.route import RouteCreate, RouteUpdate, RouteCompleteCreate, RouteScheduleCreate, RouteWizardCreate
 from app.models.route import Route
 from app.models.stop import Stop
 from app.models.bus import Bus
